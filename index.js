f7a17e4841daf15aaecce802f3fe73b5f742d04f
Warszla <gsergiobenjamin213@gmail.com>
2026-03-13 21:27:06 -0500

    Update index.js

--- index.js
+++ index.js
@@ -12,39 +12,38 @@ ClientUserSettingManager.prototype._patch = function (data) {
 
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('⚔️ V73 - FIXED & ACTIVE ⚔️'));
+app.get('/', (req, res) => res.send('⚔️ V74 - DIRECT ATTACK ACTIVE ⚔️'));
 app.listen(process.env.PORT || 8080);
 
 // --- CONFIGURACIÓN DE IDS ---
-const SERVER_SIN_AUTOMOD = "1239701315580592148"; 
-const ID_CANAL_FORZADO = "1239719951435304960"; 
-
 const PRIORITARIOS = ["1369181247896817685", "1369174478596345897", "1369174476574687243"];
 const OBJETIVOS_IDS = ["1447142638326120458", "1457144912561832182", "1479748142722191514", "1479755930483691610", "1457984414121459856"];
 
-const MI_MENSAJE_LARGO = `.t cputiñagachatuber <@1425209744603218020> <@1195495311045558272> <@1369070242684473485> <@984956970014486528> <@1072352198836621385> CULOMBIANO ARGENCHANGAS <@1435003733393281055> <@1400251089361567885> <@1429177016703516764> DANIELA <@1438314463970328578> <@1384045898958508085> <@1446586105553227807> <@1452154841676775567> <@957014429822750771> <@1423439348430405722> <@1455444386421674007> <@765971830442819674> <@1394021604127936772> <@1452533908699611236> <@1438662990021922869> <@1459077041637953651> <@1468117706099396816> <@1467397075204309034> <@1466878653932634195> <@1458314974794616902> <@1403986874153832550> <@1470913175401533543> <@1464354934785839155> <@1394023020896714762> <@1399500980889976902> <@1470230646529069086> <@1462897561894649876> @everyone DANIELA <@1386330375952793723> <@1399500980889976902> <@1466878653932634195> \n\nhttps://files.catbox.moe/d0wcx2.mp4`;
+// El video que quieres que se vea siempre
+const VIDEO_LINK = "https://files.catbox.moe/d0wcx2.mp4";
 
+// Bardeos directos (sin el .t para que Dyno no te joda)
 const MIS_BARDEOS = [
-    ".t warszla JSKSJDJDJD MALDITA MONCLOVEÑA",
-    ".t v14 HEY CHE TE ARDE ESTA PERR4",
-    ".t cputiñagachatuber MAMITA CEJOTORRA",
-    ".t cejotiñaandgamami BRAZOS MÁS LONJUDOS",
-    ".t cejotiñagolpeada MALDITA Q QUIERE EDITAR",
-    ".t cejotorra MAMELE MÁS MEJICHANGA",
-    ".t lorda CJOTORRA VIENDO TODO con su cara",
-    ".t frijolera FRIJOLERA DILE DOMADORA",
-    ".t joan MACH4 G4M4MITA DIRÍA LA PUTITA",
-    ".t chichuda VENGAN MEJICHANGAS DENLE TET4",
-    ".t cjotangaandgamami CEJOTORRA Y GAMAMITA",
-    ".t cejuda2 PINCHE PERRA CJOTIÑA",
-    ".t nito PERRA TIENES Q ENTENDER Q SOS",
-    ".t india LA MEJINDIA DE MICHOACAN",
-    ".t insana TE ARDIÓ LAS NALGAS INSANA",
-    ".t cputiña CHINGERO DE SEMEN EN SUS ANOS",
-    ".t kayada JDKDJDJJSS LORDA PUTITA SE CALLO",
-    ".t cjotorr4 VAGINA DE CEJOTIÑA SHE",
-    ".t nalgotanga APURATE NALGOTANGA SALVA",
-    ".t cejud4 SE LE DESCONFIGURO LA NALGA"
+    "warszla JSKSJDJDJD MALDITA MONCLOVEÑA",
+    "HEY CHE TE ARDE ESTA PERR4",
+    "MAMITA CEJOTORRA CPUTIÑAGACHATUBER",
+    "BRAZOS MÁS LONJUDOS CEJOTIÑA",
+    "MALDITA Q QUIERE EDITAR CEJOTIÑA GOLPEADA",
+    "MAMELE MÁS MEJICHANGA CEJOTORRA",
+    "CJOTORRA VIENDO TODO CON SU CARA LORDA",
+    "FRIJOLERA DILE DOMADORA",
+    "MACH4 G4M4MITA DIRÍA LA PUTITA",
+    "VENGAN MEJICHANGAS DENLE TET4 CHICHUDA",
+    "CEJOTORRA Y GAMAMITA CJOTANGA",
+    "PINCHE PERRA CJOTIÑA CEJUDA",
+    "PERRA TIENES Q ENTENDER Q SOS NITO",
+    "LA MEJINDIA DE MICHOACAN INDIA",
+    "TE ARDIÓ LAS NALGAS INSANA",
+    "CHINGERO DE SEMEN EN SUS ANOS CPUTIÑA",
+    "JDKDJDJJSS LORDA PUTITA SE CALLO",
+    "VAGINA DE CEJOTIÑA SHE CJOTORR4",
+    "APURATE NALGOTANGA SALVA",
+    "SE LE DESCONFIGURO LA NALGA CEJUD4"
 ];
 
 let botIndex = 0;
@@ -53,7 +52,7 @@ const botsReady = [];
 function crearBot(token, nombre) {
     const client = new Client({ checkUpdate: false });
     client.contador = 0;
-    client.limite = Math.floor(Math.random() * 5) + 12;
+    client.limite = Math.floor(Math.random() * 8) + 10; 
     client.enReposo = false;
 
     client.on('ready', () => {
@@ -93,14 +92,8 @@ async function scheduleNextAttack() {
                     const bardeo = MIS_BARDEOS[Math.floor(Math.random() * MIS_BARDEOS.length)];
                     const target = OBJETIVOS_IDS[Math.floor(Math.random() * OBJETIVOS_IDS.length)];
                     
-                    let finalMsg;
-                    const bypass = "** **\n"; // El truco para engañar a Dyno
-
-                    if (channel.id === ID_CANAL_FORZADO || channel.guildId === SERVER_SIN_AUTOMOD) {
-                        finalMsg = `${bypass}${MI_MENSAJE_LARGO} \`[Δ${delta}]\``;
-                    } else {
-                        finalMsg = `${bypass}${bardeo} <@${target}> \`[Δ${delta}]\``;
-                    }
+                    // Mandamos bardeo + mención + video directamente
+                    const finalMsg = `${bardeo} <@${target}>\n${VIDEO_LINK}\n\`[Δ${delta}]\``;
                     
                     try {
                         await channel.send(finalMsg);
@@ -111,7 +104,7 @@ async function scheduleNextAttack() {
         } catch (e) {}
     }
 
-    const nextAttackDelay = Math.floor(Math.random() * 25000) + 45000;
+    const nextAttackDelay = Math.floor(Math.random() * 30000) + 40000;
     setTimeout(scheduleNextAttack, nextAttackDelay);
 }
 
